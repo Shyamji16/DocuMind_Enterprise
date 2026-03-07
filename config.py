57629c7d7127853d11d016a349c4c477c28679df
Shyamji16 <tiwarishyamji1515@gmail.com>
2026-03-07 16:16:08 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+INDEX_NAME = os.getenv("PINECONE_INDEX")
